17262b49d2bbcc576624483a63512981a8460e3e
Textgithubac <162041640+Textgithubac@users.noreply.github.com>
2024-03-08 14:55:33 +0530

    Add files via upload

---
+++ Day2/D02P05.java
@@ -0,0 +1,21 @@
+package com.Day2;
+
+import java.util.Scanner;
+
+public class D02P05 {
+	
+	
+	    public static void main(String[] args) {
+	        Scanner scanner = new Scanner(System.in);
+	        System.out.print("Enter a number: ");
+	        int number = scanner.nextInt();
+
+	        if (number % 5 == 0) {
+	            System.out.println("Number divisible by 5");
+	        } else {
+	            System.out.println("Number is not divisible by 5");
+	        }
+	    }
+}
+
+
