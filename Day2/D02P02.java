752bf0ecbd728b7baaf0c1e9c88a8f25cc5b1fd8
Textgithubac <162041640+Textgithubac@users.noreply.github.com>
2024-03-09 11:13:45 +0530

    Add files via upload

---
+++ Day2/D02P02.java
@@ -0,0 +1,23 @@
+package com.Day2;
+
+import java.util.Scanner;
+
+public class D02P02 {
+	   public static void main(String[] args) {
+	        Scanner scanner = new Scanner(System.in);
+	        System.out.print("Enter a number: ");
+	        int number = scanner.nextInt();
+
+	        System.out.print("Even numbers less than or equal to " + number + ": ");
+	        if (number >= 2) {
+	            for (int i = 2; i <= number; i++) {
+	                if (i % 2 == 0) {
+	                    System.out.print(i + " ");
+	                }
+	            }
+	        } else {
+	            System.out.println("No even numbers less than or equal to " + number);
+	        }
+	    }
+
+}
