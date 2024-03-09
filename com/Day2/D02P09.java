a6de26b4379d932e5e8bdba676041ebb1b3df46a
Textgithubac <162041640+Textgithubac@users.noreply.github.com>
2024-03-09 11:09:14 +0530

    Add files via upload

---
+++ com/Day2/D02P09.java
@@ -0,0 +1,23 @@
+package com.Day2;
+
+import java.util.Scanner;
+
+public class D02P09 {
+	
+	    public static void main(String[] args) {
+	        Scanner scanner = new Scanner(System.in);
+	        System.out.print("Enter a number: ");
+	        int n = scanner.nextInt();
+
+	        if (n > 0) {
+	            for (int i = 1; i <= n; i++) {
+	                int cube = i * i * i;
+	                System.out.print(cube + " ");
+	            }
+	            System.out.println();
+	        } else {
+	            System.out.println("Invalid Input");
+	        }
+	    }
+
+}
---
+++ com/Day2/D02P11.java
@@ -0,0 +1,26 @@
+package com.Day2;
+
+import java.util.Scanner;
+
+public class D02P11 {
+	
+	
+	    public static void main(String[] args) {
+	        Scanner scanner = new Scanner(System.in);
+	        System.out.print("Enter the number of rows: ");
+	        int rows = scanner.nextInt();
+
+	        if (rows > 0) {
+	            for (int i = 1; i <= rows; i++) {
+	                for (int j = 1; j <= i; j++) {
+	                    System.out.print(i);
+	                }
+	                System.out.println();
+	            }
+	        } else {
+	            System.out.println("Invalid input");
+	        }
+	    }
+}
+
+
